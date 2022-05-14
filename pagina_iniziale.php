b06983db44c7845f170c486416751336dfae1cf8
Adriana1102 <93667862+Adriana1102@users.noreply.github.com>
2022-05-14 19:39:40 +0200

    Add files via upload
    logo scorrevole

---
+++ pagina_iniziale.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Toylandia Giochi e Giocattoli</title>
+    <marquee scrollamount="15" onmouseover="this.stop()"  onmouseout="this.start()" loop="" style='position:fixed; top:0; right:0; z-index:1000; width:100%;'>
+    <img src='logo.jpeg' border="0" width="175"/></marquee>
+
+</head>
+<body>
+<?php
+?>
+</body>
+</html>
