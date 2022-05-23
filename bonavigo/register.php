26f7484becc9cdfd21648bd7d2adca8b6a756557
KeviBona <47268645+KeviBona@users.noreply.github.com>
2022-05-23 11:08:26 +0200

    Update register.php

--- bonavigo/register.php
+++ bonavigo/register.php
@@ -5,12 +5,13 @@ require_once('config.php');
 $email = $connesione->real_escape_string($_POST['email']);
 $username = $connesione->real_escape_string($_POST['username']);
 $password = $connesione->real_escape_string($_POST['password']);
+$hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
 $sql = "INSERT INTO utenti (email, username, password) VALUES ('$email', '$username', '$password')";
-if($connesione->query($sql) === true){
+if($connection->query($sql) === true){
     echo "Registrazione effettuata con successo!";
 }else{
-    echo "Errore durante la registrazione utente $sql. " . $connesione->error;
+    echo "Errore durante la registrazione utente $sql. " . $connection->error;
 }
 
 ?>
