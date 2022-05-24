65aeee262a1cfc1e0beeecc08b68cfe66bc03335
Albe-Mago <95043703+Albe-Mago@users.noreply.github.com>
2022-05-24 10:19:24 +0200

    Add files via upload

---
+++ Loginis/home.php
@@ -0,0 +1,55 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<style>
+        h1{
+            color: yellow;
+            size: 30px;
+        }
+        body {
+            font-size: 32px;
+            display : flex;
+            justify-content: center;
+            background-image: url(sfondo.jpg);
+             }
+        div{
+            color: white;
+            display: flex;
+            flex-direction: column;
+            width: 300px;
+        }
+        
+        form > input{
+            margin-bottom: 20px;
+            border-radius: 10px; 
+        } 
+        input { 
+	font: 20px 'Open Sans', sans-serif;
+    border: px;
+    padding: 2px;
+    display: block;
+    width: 96%; 
+    cursor:pointer;
+}
+    button{
+    border-radius: 15px;
+	margin-bottom: 10px;
+    width: 100px;
+
+}
+    </style> 
+<body>
+    <div>
+        <br><br><br><br>
+    <h1> Home</h1>
+        <button><a href="prova1.php">Registrati</a>
+        <button><a href="accedi.php">Accedi</a></button>
+    </div>
+</body>
+</html>
